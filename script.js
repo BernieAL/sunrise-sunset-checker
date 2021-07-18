39bdcc3da0fe577582aaa5451533e18d542781f4
BernieTheBear <balmanzar883@gmail.com>
2021-07-18 18:37:32 -0400

    Add files via upload
    Fixed disconnected materialize cdn
    Added header to display location name above sunset and sunrise card
    
    Issue:
    Unknown timezone results -> not specific to location timezone. Times are being returned in EST???
    Prev searches and results are not cleared on new searches

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ window.onload = function() {
                     Done   
  */   
 
-
+    var cityName;
 
     document.querySelector('#searchButton').addEventListener('click',(e)=>{
         e.preventDefault();
@@ -23,12 +23,12 @@ window.onload = function() {
         
         const urlIn = "https://api.mapbox.com/geocoding/v5/mapbox.places/"+locationEntered+".json?access_token=pk.eyJ1IjoiYmh1cm5hbCIsImEiOiJjazgwdHk2bDAwMzd0M21udGRrczhyejVqIn0.rt0SsWU6VcEm_3-FVhN82w"
         
-       
+        getGeoCoordinates(urlIn)
 
       fetch (urlIn)
         .then(response => response.json())
         .then(data =>{
-              
+
             let latLong = {
                 query: data.query[0],
                 lat: data.features[0].geometry.coordinates[0],
@@ -36,7 +36,7 @@ window.onload = function() {
             }
             return latLong
         }).then(function(latLong){
-
+            cityName = latLong.query
             lat = latLong.lat
             long = latLong.long
 
@@ -49,30 +49,35 @@ window.onload = function() {
                         sunset :(data.results.sunset )
                     }
 
-                    
+                    renderResultRow(cityName)
                     updateSunrise(times.sunrise)
                     updateSunset(times.sunset)
                 })
         })
-
-
-    })  
+})  
     
  
 //=====================================================
 /* Helper methods - these render updated content to the sunrise and sunset cards  */
     //fill sunrise
+
+    const renderResultRow = function(cityName){
+        const ResultRow = document.querySelector('#locationDisplayHeader')
+        const cityElement = document.createElement('h2')
+        cityElement.textContent = cityName.toUpperCase()
+        ResultRow.appendChild(cityElement)
+    }
     const updateSunrise = function(time){
-        const p = document.createElement('p')
-        p.textContent = time
-        document.querySelector('#sunrise-card').appendChild(p)
+        const h3 = document.createElement('h3')
+        h3.textContent = time
+        document.querySelector('#sunrise-card').appendChild(h3)
     }
 
     //fill sunset
     const updateSunset = function(time){
-        const p = document.createElement('p')
-        p.textContent = time
-        document.querySelector('#sunset-card').appendChild(p)
+        const h3 = document.createElement('h3')
+        h3.textContent = time
+        document.querySelector('#sunset-card').appendChild(h3)
     }
 
 
